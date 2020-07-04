fafa991bf1fe6ea5b887f238dc59f326b25c3906
IgorWilbert <igor.wilbert@gmail.com>
2020-07-04 16:00:49 -0300

    Creates Menu component

---
+++ src/components/Menu/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Menu = () => {
+  return (
+    <div>
+      <button>Campanha de apadrinhamento</button>
+      <button>Quero adotar um animal</button>
+      <button>Brechó do Bem</button>
+      <button>Entrevista - Moreno Pet Blog</button>
+      <button>Apadrinhamento - Tratamento Mara</button>
+    </div>
+  );
+};
+
+export default Menu;
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,11 +1,13 @@
 import React from "react";
 
 import Gallery from "../../components/Gallery";
+import Menu from "../../components/Menu";
 
 const Home = () => {
   return (
     <>
       <h1>Home</h1>
+      <Menu />
       <Gallery />
     </>
   );
