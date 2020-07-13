4ab870e26bfccea81d89c40902e34087ad12ae37
IgorWilbert <igor.wilbert@gmail.com>
2020-07-13 09:41:36 -0300

    Fixes style of Facebook page

--- src/pages/Facebook/index.js
+++ src/pages/Facebook/index.js
@@ -1,26 +1,35 @@
 import React from "react";
 
 const Facebook = () => (
-  <div>
-    <div
-      class="fb-page"
-      data-href="https://www.facebook.com/nosdobem.protecaoanimal/"
-      data-tabs="timeline"
-      data-width=""
-      data-height=""
-      data-small-header="false"
-      data-adapt-container-width="true"
-      data-hide-cover="false"
-      data-show-facepile="true"
-    >
-      <blockquote
-        cite="https://www.facebook.com/nosdobem.protecaoanimal/"
-        class="fb-xfbml-parse-ignore"
+  <div
+    style={{
+      width: "100%",
+      display: "flex",
+      justifyContent: "center",
+      alignItems: "center",
+    }}
+  >
+    <div style={{ minWidth: "340px" }}>
+      <div
+        class="fb-page"
+        data-href="https://www.facebook.com/nosdobem.protecaoanimal/"
+        data-tabs="timeline"
+        data-width=""
+        data-height=""
+        data-small-header="false"
+        data-adapt-container-width="true"
+        data-hide-cover="false"
+        data-show-facepile="true"
       >
-        <a href="https://www.facebook.com/nosdobem.protecaoanimal/">
-          Nós do Bem
-        </a>
-      </blockquote>
+        <blockquote
+          cite="https://www.facebook.com/nosdobem.protecaoanimal/"
+          class="fb-xfbml-parse-ignore"
+        >
+          <a href="https://www.facebook.com/nosdobem.protecaoanimal/">
+            Nós do Bem
+          </a>
+        </blockquote>
+      </div>
     </div>
   </div>
 );
